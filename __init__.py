a318f6daa1fc67f72697b0ff8decdf1ebe6a7195
Joachim Mortensen <joachim.mortensen@gmail.com>
2013-10-25 18:52:22 +0200

    Adding __init__.py.

---
+++ __init__.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python -OB
+# -*- coding: utf-8 -*-
+# Version: Sat 4 May 2013
+#   * Initial build.
+#
+
+"""
+Module containing the auxiliary function setup_and_parse()
+"""
+
+import argparse
+
+
+def setup_and_parse(desc, arglist):
+    """
+    Returns namespace with parsed arguments.
+
+    Example:
+    --------
+
+    arglist = [
+        (('hours'), dict(type=float, help='Number of hours')),
+        (('-b', '--beg'), dict(type=int, default=26, help='First included week number')),
+        (('-e', '--end'), dict(type=int, default=35, help='Last included week number')),
+        (('-d', '--display'), dict(action='store_true', help='Display the months')),
+    ]
+
+    """
+
+    # Initialise parser
+    parser = argparse.ArgumentParser(description=desc)
+    for flags, kwargs in arglist:
+        parser.add_argument(*flags, **kwargs)
+
+    # Parse arguments
+    return parser.parse_args()
+
+
