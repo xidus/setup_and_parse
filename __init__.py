be43209507eacb056a85b5b8a3eff1dfbb4339a8
Joachim Mortensen <joachim.mortensen@gmail.com>
2014-07-31 10:56:25 +0200

    Added date-type parser.

--- __init__.py
+++ __init__.py
@@ -1,23 +1,67 @@
 #!/usr/bin/python -OB
 # -*- coding: utf-8 -*-
+# Version: Sun 9 Feb 2014
+#   * Added conversion function for type=dt.date
+#
 # Version: Sat 4 May 2013
 #   * Initial build.
 #
 
 """
-Module containing the auxiliary function setup_and_parse()
+Auxiliary functions for easy setup and parsing with Python's argparse module.
+
+Main work horse is the the function `setup_and_parse()`
+
+
 """
 
 import argparse
+
+
+def sap_date(string):
+    import datetime as dt
+    fmt = '%Y-%m-%d'
+    try:
+        value = dt.datetime.strptime(string, fmt).date()
+    except:
+        msg = '{} does not conform to ISO format (\'{}\')'.format(string, fmt)
+        raise argparse.ArgumentTypeError(msg)
+    return value
+
+
+def sap_datetime(string):
+
+    import datetime as dt
+
+    fmts = [
+        '%Y-%m-%d',
+        '%Y-%m-%dT%H:%M',
+        '%Y-%m-%dT%H:%M:%S',
+        '%Y-%m-%dT%H:%M:%S.%f',
+    ]
+
+    value = None
+    for fmt in fmts:
+        try:
+            value = dt.datetime.strptime(string, fmt)
+        except:
+            continue
+
+    if value is None:
+        msg = '{} does not conform to ISO formats (\'{}\')'.format(
+            string, ', '.join(fmts)
+        )
+        raise argparse.ArgumentTypeError(msg)
+
+    return value
 
 
 def setup_and_parse(desc, arglist):
     """
     Returns namespace with parsed arguments.
 
-    Example:
-    --------
-
+    Example
+    -------
     arglist = [
         (('hours'), dict(type=float, help='Number of hours')),
         (('-b', '--beg'), dict(type=int, default=26, help='First included week number')),
